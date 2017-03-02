ec0293409e91328aaf4906a2eef7f9a7a8d9bfbd
TheAbraxas98 <nekron_warrior@live.com>
2017-03-03 01:06:45 +0200

    Fixed pytesseract full path

--- textify/core/text_converter.py
+++ textify/core/text_converter.py
@@ -11,10 +11,11 @@ class TextConverter:
         initial = cv2.imread(filename)
         im_gray = cv2.cvtColor(initial, cv2.COLOR_BGR2GRAY)
         ret, thresh = cv2.threshold(im_gray, 200, 255, cv2.THRESH_BINARY_INV)
-        temp_filename = os.path.join('user_files', str(uuid.uuid4()) + 'temp.jpg')
+        temp_filename = os.path.join(os.path.join(os.getcwd(), 'user_files'), str(uuid.uuid4()) + 'temp.jpg')
         cv2.imwrite(temp_filename, thresh)
 
         img = Image.open(temp_filename)
+        pytesseract.image_to_string()
         text = pytesseract.image_to_string(img, lang="eng")
         text = os.linesep.join([s for s in text.splitlines() if s])
         os.remove(temp_filename)
